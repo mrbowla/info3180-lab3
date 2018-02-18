98fcc821b76ebd1ac51487d7e2bc3265deddc6f5
Rahmeesh Bowla <rahmeesh.bowla@mymona.uwi.edu>
2018-02-18 01:24:27 +0000

    Lab 3 complete

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'omaiwa'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '2525'
-app.config['MAIL_USERNAME'] = 'dcdef9106e0624'
-app.config['MAIL_PASSWORD'] = 'ea264780ae216c'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 mail=Mail(app)
 from app import views
